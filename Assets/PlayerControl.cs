ae58de2aa762f466543274c81eab3e86c899b25d
Michael Collins <duhprgrmman@gmail.com>
2016-01-30 15:48:47 -0800

    Hopefully ignored files are ok now

--- Assets/PlayerControl.cs
+++ Assets/PlayerControl.cs
@@ -13,12 +13,21 @@ public class PlayerControl : MonoBehaviour {
 
 	public bool isGrab = false;
 
+	public OpponentControl opponent;
+
 	public Vector2 initialMousePos;
 
+	public enum phase
+	{
+		approach,
+		shake
+	}
+
+	public phase currentPhase = phase.approach;
 
 	private GripBar gripBar;
 	private SpriteRenderer handSprite;
-	
+
 	// Use this for initialization
 	void Start () {
 		Debug.Log ("Hello");
@@ -27,14 +36,27 @@ public class PlayerControl : MonoBehaviour {
 
 		gripBar = Object.FindObjectOfType<GripBar> ();
 		handSprite = GetComponentInChildren<SpriteRenderer> ();
+		opponent = Object.FindObjectOfType<OpponentControl> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		switch (currentPhase) {
+		case phase.approach:
+			DoMovementPhase();
+			break;
+
+		case phase.shake:
+			DoShakePhase();
+			break;
+		}
+	}
+
+	void DoMovementPhase () {
 		Vector2 mousePos = Camera.main.ScreenPointToRay (Input.mousePosition).origin;
 		Vector2 pos = transform.position;
 		pos.y = mousePos.y; // * verticalVelocityScale;
-
+		
 		// Add forward velocity when the player hold accelerator
 		if (Input.GetButton ("Accelerate")) {
 			speed += forwardAcceleration * Time.deltaTime;
@@ -44,13 +66,36 @@ public class PlayerControl : MonoBehaviour {
 		}
 		speed -= speed * friction;
 		pos.x += speed * Time.deltaTime;
-
+		
 		// Handle gripping
 		if (Input.GetButtonDown ("Grip")) {
-			isGrab = true;
+			AttemptGrip();
+		}
+		
+		transform.position = pos;
+	}
+
+	void DoShakePhase() {
+
+	}
+
+	void AttemptGrip() {
+		// Find the center and range of both the grip points
+		Transform myGripPointObj = transform.FindChild ("GripPoint");
+		Transform otherGripPointObj = opponent.transform.FindChild ("GripPoint");
+		Vector2 myGripPos = myGripPointObj.TransformPoint (myGripPointObj.GetComponent<CircleCollider2D> ().offset);
+		Vector2 otherGripPos = otherGripPointObj.TransformPoint (otherGripPointObj.GetComponent<CircleCollider2D> ().offset);
+
+		if (Vector2.Distance(myGripPos, otherGripPos) < myGripPointObj.GetComponent<CircleCollider2D> ().radius) {
+			// We're in range!!
+			speed = 0;
+			handSprite.color = Color.green;
+			// Move to next phase
+			currentPhase = phase.shake;
+		} else {
+			// Failed to grip correctly
 			handSprite.color = Color.red;
 		}
 
-		transform.position = pos;
 	}
 }
