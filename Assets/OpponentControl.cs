4d8b4343495f48bb4dce28ea735704bc6c8a355a
Michael Collins <duhprgrmman@gmail.com>
2016-01-30 17:24:04 -0800

    Added autocenter method to opponent control

--- Assets/OpponentControl.cs
+++ Assets/OpponentControl.cs
@@ -28,7 +28,25 @@ public class OpponentControl : MonoBehaviour {
 
 	void DoApproach() {
 		t += tSpeed * Time.deltaTime;
+		t = Mathf.Clamp01 (t);
 		Vector2 p = path.GetPointAt (t);
 		transform.position = p;
+	}
+
+	void AutoCenter() {
+		StartCoroutine (DoAutoCenter ());
+	}
+
+	// Coroutine that moves the hand towards the center of the screen
+	IEnumerable DoAutoCenter() {
+		bool complete = false;
+		float maxCenterSpeed = 0.1f; // Should be a pub var!
+
+		while (Vector2.Distance(transform.position, Vector2.zero) > Mathf.Epsilon) {
+			// Move towards the center
+			transform.position = Vector2.MoveTowards (transform.position, Vector2.zero, maxCenterSpeed * Time.deltaTime);
+			yield return null;
+		}
+		transform.position = Vector2.zero;
 	}
 }
