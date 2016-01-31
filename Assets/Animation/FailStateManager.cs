b6f2c1f7719c815603be9da848576000df0b8609
Michael Collins <duhprgrmman@gmail.com>
2016-01-31 14:22:03 -0800

    Added alex's failscreen

---
+++ Assets/Animation/FailStateManager.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FailStateManager : MonoBehaviour {
+
+	//public MasterGame handStage;
+
+	Animator anim;
+	public bool buttonPlayAgain = false;
+
+	void Awake(){
+		anim = GetComponent<Animator>();
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (MasterGame.instance.currentHandStage == MasterGame.handStage.Failure){
+			anim.SetTrigger("FailScreen");
+		}
+		//if (buttonPlayAgain == true){
+		//	Application.LoadLevel("ProjHandshakeTest0.03");
+		//}
+	}
+}
