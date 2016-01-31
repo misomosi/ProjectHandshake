4578f2fc506150de38fafbde63440c55432f74e1
Alex Smirnov <asmirno1@ucsc.edu>
2016-01-30 17:13:47 -0800

    Added game state manager

---
+++ Assets/Grip.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Grip : MonoBehaviour {
+
+	//Variables for the grip activity during handStage2
+	[Header("Grip")]
+	public float gripStrength;
+	public float gripMash = 12;
+	public float gripTargetMin;
+	public float gripTargetMax;
+	public float gripAbsoluteMin = 0;
+	public float gripAbsoluteMax;
+	public bool gripLost = false;
+	int handStage;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		handStage = (int)GameObject.Find("GameStateControl").GetComponent<MasterGame>().currentHandStage;
+
+		if(handStage == 2){
+			if (Input.GetKeyDown("space") == true){
+				gripStrength += gripMash;
+			}
+			//if (||)
+		}
+	}
+}
---
+++ Assets/MasterGame.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MasterGame : MonoBehaviour {
+
+	// Use this for initialization
+
+
+	//main game variables used to switch between different levels and control game progression.
+
+	public enum gameStage {Intro,Tutorial,Baby,HighSchool,Boss,GameOver};
+	public enum handStage {ReadyGo,Joust,AutoCenter,Shake,Success,Failure};
+	[Header("MainGame")]
+	public bool gameOver = false;
+	public gameStage currentGameStage;
+	public handStage currentHandStage;
+
+
+	//[Header("Grip")]
+	bool gripLost;
+
+	[Header("PlayerHand")]
+	bool isGrab;
+	float handShakeX;
+	public float handAutoDragSpeed;
+	public PlayerControl player;
+	bool autoCenterComplete = false;
+
+	[Header("Screen Constants")]
+	public float centerX = 0;
+	public float centerY = 0;
+
+
+	void Start () {
+		player = Object.FindObjectOfType<PlayerControl>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(gameOver==false){
+
+			//tests for currentGameStage
+
+				isGrab = player.isGrab;
+				if (isGrab == true){
+					currentHandStage = handStage.AutoCenter;
+					player.SendMessage("autoCenter");
+				}
+
+				/*
+				if (autocenter is complete){
+					currentHandStage = handStage.Shake;
+				}
+				*/
+
+
+
+		}
+	}
+
+	//functions here, if needed
+
+}
