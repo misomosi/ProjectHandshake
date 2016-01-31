2a06471d19a033f600a138c9f178a5fa5b285102
Michael Collins <duhprgrmman@gmail.com>
2016-01-30 16:17:08 -0800

    Added opponent curve follow

--- Assets/OpponentControl.cs
+++ Assets/OpponentControl.cs
@@ -3,13 +3,32 @@ using System.Collections;
 
 public class OpponentControl : MonoBehaviour {
 
+	private BezierCurve path;
+
+	public enum phase
+	{
+		approach,
+		shake
+	}
+	public float tSpeed = 1.0f;
+	public phase currentPhase = phase.approach;
+	private float t = 0;
+
 	// Use this for initialization
 	void Start () {
-	
+		path = GameObject.Find ("OpponentCurve").GetComponent<BezierCurve> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if (currentPhase == phase.approach) {
+			DoApproach ();
+		}
+	}
+
+	void DoApproach() {
+		t += tSpeed * Time.deltaTime;
+		Vector2 p = path.GetPointAt (t);
+		transform.position = p;
 	}
 }
